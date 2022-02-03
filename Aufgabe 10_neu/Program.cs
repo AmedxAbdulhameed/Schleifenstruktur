8d4d521f91c2216d6f33141a1822c8c2c87c17fc
Amed Abdulhameed <amed.a@gso.schule.koeln>
2022-02-03 11:43:20 +0100

    Aufgabe 10 neu eingefügt

---
+++ Aufgabe 10_neu/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Aufgabe_10_neu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben Sie die Höhe des Baumes an!!");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int FS = (n - 1) - i;
+                int S = 1 + (i * 2);
+
+                #region Freiestellen
+                for (int q = 0; q < FS; q++)
+                {
+                    Console.Write(" ");
+                }
+                #endregion
+
+                #region Sterne
+                for (int q = 0; q < S; q++)
+                {
+                    Console.Write("*");
+                }
+                #endregion
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
