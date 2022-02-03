2346c47b6e09bf0a1a9a76d8d76bcbb49b28f175
Amed Abdulhameed <amed.a@gso.schule.koeln>
2022-02-03 12:58:09 +0100

    Übung zu Klassenarbeit

---
+++ Übung zu Klassenarbeit/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Übung_zu_Klassenarbeit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string J,G,Z,KZ,B;
+
+            J = "Jahr";
+            G = "Guthaben";
+            Z = "Zinsen";
+            KZ = "K.Zinsen";
+            B = "Bilanz";
+
+            Console.WriteLine("Geben Sie Ihr Kapital ein");
+
+            double K = Convert.ToInt32(Console.ReadLine());
+            double p = Convert.ToInt32(Console.ReadLine());
+            double n = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("{0,3} {1,10} {2,8} {3,10} {4,8}",J,G,Z,KZ,B);
+            Console.WriteLine("--------------------------------------------");
+
+            Console.WriteLine("{0,13}{1,2}{2,22}", K,n,p);
+            
+
+
+
+        }
+    }
+}
