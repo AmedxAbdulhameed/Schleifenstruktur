39b0cc80e9e0192ef933c8840b4d4cc67d1d9fac
Amed Abdulhameed <amed.a@gso.schule.koeln>
2022-02-09 09:14:45 +0100

    fertig

---
+++ Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Finanztabelle
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                double k = Convert.ToDouble(Console.ReadLine());
+                double p = Convert.ToDouble(Console.ReadLine());
+                double n = Convert.ToDouble(Console.ReadLine());
+
+
+                Console.WriteLine($"{"Jahr",4} {"Guthaben",10} {"Zinsen",8} {"K.Zinsen",10} {"Bilanz",8}");
+
+                for (int i = 1; i <= n; i++)
+                {
+
+                    Console.WriteLine($"{i,4} {k,10:F2} {"Zinsen",8} {"K.Zinsen",10} {"Bilanz",8}");
+                }
+            }
+        }
+    }
+
+  
--- Übung zu Klassenarbeit/Program.cs
+++ Übung zu Klassenarbeit/Program.cs
@@ -6,6 +6,7 @@ namespace Übung_zu_Klassenarbeit
     {
         static void Main(string[] args)
         {
+            double K = 0, z = 0, kz = 0, kn = 0, b=0;
             string J,G,Z,KZ,B;
 
             J = "Jahr";
@@ -15,22 +16,34 @@ namespace Übung_zu_Klassenarbeit
             B = "Bilanz";
 
             Console.WriteLine("Geben Sie Ihr Kapital ein");
-
-            double K = Convert.ToInt32(Console.ReadLine());
+            K = Convert.ToInt32(Console.ReadLine());
+            kn = K;
+            Console.WriteLine("Geben Sie Zinssatz in prozent ein");
             double p = Convert.ToInt32(Console.ReadLine());
+           
+            
+            Console.WriteLine("Geben Sie Ihre Jahre ein");
             double n = Convert.ToInt32(Console.ReadLine());
 
 
 
-            Console.WriteLine("{0,3} {1,10} {2,8} {3,10} {4,8}",J,G,Z,KZ,B);
-            Console.WriteLine("--------------------------------------------");
-
-            Console.WriteLine("{0,13}{1,2}{2,22}", K,n,p);
+            Console.WriteLine("{0,3}    {1,10}    {2,8}    {3,10}    {4,8}", J,G,Z,KZ,B);
+            Console.WriteLine("-------------------------------------------------------");
 
 
-            
 
+            for (int i = 1; i <= n; i++)
+            {
+                z = kn * (p/100);
+                kz = kz + z;
+                b = kn + z;
+                Console.WriteLine($"{i,3} | {kn,10:F2}€ | {z,8:F2}€ | {kz,10:F2}€ | {b,8:F2}€");
 
+                kn = kn + z;
+            }
+            Console.WriteLine("-------------------------------------------------------");
+
+          
 
         }
     }
