424a2c1f53064ded637589baf6046eb29791ccca
Amed Abdulhameed <amed.a@gso.schule.koeln>
2022-03-10 11:59:07 +0100

    Datenfeld Array Neu

--- Datenfeld Array/Program.cs
+++ Datenfeld Array/Program.cs
@@ -8,17 +8,29 @@ namespace Datenfeld_Array
         {
             int k = 20; 
             int[] n = new int[k];
+            double summe = 0;
+
             for (int i = 0; i < k; i++)
             {
-                  Console.WriteLine($"Geben Sie die {i} Note ein");
+                Console.WriteLine($"Geben Sie die {i} Note ein");
                 n[i] = Convert.ToInt32(Console.ReadLine());
-     
-            }
-            for(int p = 0 ; p < n.Length; p++ )
-            {
-                Console.WriteLine(n[p]);
+
 
             }
+
+            Console.Clear();
+            
+            for(int p = 0 ; p < n.Length; p++ )
+            {
+                summe = summe + n[p];
+                Console.WriteLine(n[p]      );
+
+
+
+
+            }
+            summe = summe / k;
+            Console.WriteLine($"Der Durschnit Betregt {summe:F2}/0");
 
         }
     }
