ca107a1d50c5b3a73e1284a6516e8735333329b7
Amed Abdulhameed <amed.a@gso.schule.koeln>
2022-03-23 14:24:41 +0100

    neu Klassenarbeit

--- Klassenarbeit/Program.cs
+++ Klassenarbeit/Program.cs
@@ -6,7 +6,34 @@ namespace Klassenarbeit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Schrittgröße");
+
+            double SG = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Maximalwert");
+            double MW = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{"Widerstand",10}{"Strom",10}");
+
+
+
+
+
+            if (MW > 150)
+            {
+                for (double R = 150; R <= MW; R += SG)
+                {
+                    Console.WriteLine($"{R,7:F2}Ohm{(230 / R),9:F2}A");
+                }
+            }
+            else if (MW < 150)
+            {
+                for (double R = 150; R >= MW; R -= SG)
+                {
+                    Console.WriteLine($"{R,7:F2}Ohm{(230 / R),9:F2}A");
+                }
+            }
+
+
         }
     }
 }
