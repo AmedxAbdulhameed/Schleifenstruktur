a5dfdac0dec8936cc5afe3b3dd53e61ad8d4784c
Amed Abdulhameed <amed.a@gso.schule.koeln>
2022-04-07 12:53:49 +0200

    Hotel_Methode neu

--- Hotel Methode/Program.cs
+++ Hotel Methode/Program.cs
@@ -4,7 +4,42 @@ namespace Hotel_Methode
 {
     class Program
     {
-        static void Main()
+
+        static (bool, short, string, string, short, double) GetData(short diff, short[] zn, string[] vn, string[] nn, short[] ün, double[] rb)
+        {
+
+            bool flag = false;
+            int index = 0;
+            for (int i = 0; i < zn.Length; i++)
+            {
+                if (diff == zn[i])
+                {
+                    index = i;
+                    flag = true;
+                }
+            }
+            return (flag, zn[index], vn[index], nn[index], ün[index], rb[index]);
+        }
+
+   
+        static (bool, short, string, string, short, double) GetData(string diff, short[] zn, string[] vn, string[] nn, short[] ün, double[] rb)
+        {
+            {
+                bool flag = false;
+                int index = 0;
+                for (int i = 0; i < nn.Length; i++)
+                {
+                    if (diff == nn[i])
+                    {
+                        index = i;
+                        flag = true;
+                    }
+                }
+                return (flag, zn[index], vn[index], nn[index], ün[index], rb[index]);
+
+            }
+        }
+        static void Main(string[] args)
         {
             do
             {
@@ -13,89 +48,52 @@ namespace Hotel_Methode
                 string[] Nachname = { "Lennon", "McCartney", "Harrison", "Starr" };
                 short[] übernachtung = { 1, 4, 12, 3 };
                 double[] rechnungsbetrag = { 82.46, 329.84, 989.52, 247.38 };
-
+                bool flag = false;
                 Console.WriteLine("Bitte geben Sie Nachnamen oder Zimmernummer ein");
                 string eingabe_str = Console.ReadLine();
                 short eingabe_short;
 
-                bool flag = short.TryParse(eingabe_str, out eingabe_short);
+                 flag = short.TryParse(eingabe_str, out eingabe_short);
 
                 if (flag == true)
                 {
-                    var daten = GetData(eingabe_str, zimmernummer, vorname, Nachname, übernachtung, rechnungsbetrag);
+                    var daten = GetData(eingabe_short, zimmernummer, vorname, Nachname, übernachtung, rechnungsbetrag);
                     if (daten.Item1 == true)
                     {
-                        Console.WriteLine($"{daten.Item2},{daten.Item3},{daten.Item4},{daten.Item5},{daten.Item6}");
+                        Console.WriteLine($"{ daten.Item2},{ daten.Item3},{ daten.Item4},{daten.Item5},{daten.Item6}");
 
                     }
                     else
                     {
-                        Console.WriteLine("fehler");
+                        Console.WriteLine("kein gegnstand gefunden");
+
 
                     }
-
-
-
-
 
                 }
                 else
                 {
-                    var daten = GetData(eingabe_short, zimmernummer, vorname, Nachname, übernachtung, rechnungsbetrag);
-                    if (daten.Item1 == false)
+                    var daten = GetData(eingabe_str, zimmernummer, vorname, Nachname, übernachtung, rechnungsbetrag);
+                    if (daten.Item1 == true)
                     {
-                      
+
 
                     }
                     else
                     {
-                        Console.WriteLine("fehler");
+                        Console.WriteLine("kein gegnstand gefunden");
+
 
                     }
 
                 }
-
 
 
             } while (true);
 
 
-        }
-        static (bool, short, string, string, short, double) GetData(short diff, short[] zn, string[] vn, string[] nn, short[] ün, double[] rb)
-        {
-            bool flag = false;
-            int index = 0;
-            for (int i = 0; i < zn.Length; i++)
-            {
-               if(diff == zn[i])
-                {
-                    index = i;
-                    flag = true;
-
-                }
 
 
-            }
-
-            return (flag, zn[index], vn[index], nn[index], ün[index], rb[index]);
-        }
-
-    static (bool, short, string, string, short, double) GetData(string diff, short[] zn, string[] vn, string[] nn, short[] ün, double[] rb)
-       {  
-            bool flag = false;
-            int index = 0;
-            for (int i = 0; i < nn.Length; i++)
-            {
-                if (diff == nn[i])
-                {
-                    index = i;
-                    flag = true;
-
-                }
-
-            }
-
-            return (flag ,zn[index], vn[index], nn[index], ün[index], rb[index]);
 
         }
 
